379f25589e1196af2617a32817e40c5ffc64ee01
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-04-14 23:55:19 +0200

    Add Section I

---
+++ Section I The Fundamentals/03 Your First PHP Tag/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tag</title>
+    </head>
+    <body>
+        <!-- Create a paragraph that uses PHP to echo 
+        any basic sentence of your choosing. 
+        Practice typing the opening and closing "<?php ?>tags. -->
+
+        <h1><?php echo "Hello, Github"; ?></h1>
+    </body>
+</html>
