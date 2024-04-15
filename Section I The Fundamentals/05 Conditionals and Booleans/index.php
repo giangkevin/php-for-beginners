a22bf484c693edbca83a2159558ac9a0963ff83e
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-04-15 02:09:42 +0200

    Add 05 Conditionals and Booleans

---
+++ Section I The Fundamentals/05 Conditionals and Booleans/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Conditionals and Booleans</title>
+        <style>
+            body{
+                display: grid;
+                place-items: center;
+                height: 100vh;
+                margin: 0;
+                font-family: sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <?php 
+            $name = "Dark Matter";
+            $read = true;
+
+            $message = "You have read $name";
+        
+        ?>
+        <h1><?= $message ?></h1>
+
+        <h2><?= "Hello World"?></h2>
+    </body>
+</html>
