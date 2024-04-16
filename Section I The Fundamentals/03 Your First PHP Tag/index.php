e037a9df7ed503aaa5f6eac01fbf99a7e5666f2c
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-04-17 01:35:27 +0200

    Move comment homework

--- Section I The Fundamentals/03 Your First PHP Tag/index.php
+++ Section I The Fundamentals/03 Your First PHP Tag/index.php
@@ -5,12 +5,13 @@
         <title>Tag</title>
     </head>
     <body>
-        <!-- Create a paragraph that uses PHP to echo 
-        any basic sentence of your choosing. 
-        Practice typing the opening and closing "<?php ?>tags. -->
+
 
         <h1><?php echo "Hello, Github"; ?></h1>
 
+        <!-- Create a paragraph that uses PHP to echo 
+        any basic sentence of your choosing. 
+        Practice typing the opening and closing "<?php ?>tags. -->
         <p><?php echo "What's good "; ?></p>
     </body>
 </html>
