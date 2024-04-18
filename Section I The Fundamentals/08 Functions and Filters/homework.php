dc770142f28e4325e6c9c14eb59546e49b546c67
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-04-18 23:02:58 +0200

    Change title

--- Section I The Fundamentals/08 Functions and Filters/homework.php
+++ Section I The Fundamentals/08 Functions and Filters/homework.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homework</title>
+    <title>Homework Function and Filters</title>
 </head>
 <body>
     <!-- Create an array of your favorites movies, including their respective release dates.
