1da21af5da0006816c58d894034f58aefff4d941
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-04-18 19:46:55 +0200

    Add 04 Variables

---
+++ Section I The Fundamentals/04 Variables/homework.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Variables</title>
+    </head>
+    <body>
+
+        <!-- Now that
+         $greeting is a variable, do the same for the noun ("World"). 
+         Then play around with changing the variable values to generate different sentences. -->
+        <h1>
+            <?php
+                $greeting = "Hello";
+                $noun = "World";
+                echo "$greeting $noun";
+            ?>
+        </h1>
+    </body>
+</html>
--- Section I The Fundamentals/04 Variables/index.php
+++ Section I The Fundamentals/04 Variables/index.php
@@ -5,15 +5,10 @@
         <title>Variables</title>
     </head>
     <body>
-        <!-- Now that
-         $greeting is a variable, do the same for the noun ("World"). 
-         Then play around with changing the variable values to generate different sentences. -->
         <h1>
             <?php
                 $greeting = "Hello";
-                $noun = "World";
-
-                echo "$greeting $noun";
+                echo "$greeting";
             ?>
         </h1>
     </body>
