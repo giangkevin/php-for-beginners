85847b47dd4d4e8e4a69494b734d61ade3ce1c22
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-04-18 19:43:54 +0200

    Add 03 Your First PHP Tag

---
+++ Section I The Fundamentals/03 Your First PHP Tag/homework.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tag</title>
+    </head>
+    <body>
+
+        <!-- Create a paragraph that uses PHP to echo 
+        any basic sentence of your choosing. 
+        Practice typing the opening and closing "<?php ?>tags. -->
+
+        <p><?php echo "What's good "; ?></p>
+    </body>
+</html>
--- Section I The Fundamentals/03 Your First PHP Tag/index.php
+++ Section I The Fundamentals/03 Your First PHP Tag/index.php
@@ -5,13 +5,6 @@
         <title>Tag</title>
     </head>
     <body>
-
-
         <h1><?php echo "Hello, Github"; ?></h1>
-
-        <!-- Create a paragraph that uses PHP to echo 
-        any basic sentence of your choosing. 
-        Practice typing the opening and closing "<?php ?>tags. -->
-        <p><?php echo "What's good "; ?></p>
     </body>
 </html>
